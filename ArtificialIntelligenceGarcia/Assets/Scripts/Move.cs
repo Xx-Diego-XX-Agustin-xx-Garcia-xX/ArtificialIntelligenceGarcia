550063ea36af97705cba88503bf20c082fc7ab0a
Xx-Diego-XX-Agustin-xx-Garcia-xX <720791@student.dusd.net>
2023-11-29 13:47:35 -0800

    Completed Cartesian Coordinates & Vectors
    Completed Cartesian Coordinates & Vectors, which added in a 3D game with NPCs gravitating towards a cubic goal.

---
+++ ArtificialIntelligenceGarcia/Assets/Scripts/Move.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour 
+{
+    public Vector3 goal = new Vector3(5, 0, 5);
+    public float speed = 1.0f;
+    void Start() 
+    {
+    }
+    void LateUpdate() 
+    {
+        this.transform.Translate(goal.normalized * speed * Time.deltaTime);
+    }
+}
